3da76507486625e3d9b7e215ea9adef96c4f62a7
hektorsoft <hektorsoft@gmail.com>
2022-04-06 22:41:49 -0300

    Adionado verificações de segurança no código info.php, mais informações em 'README.md'

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 ## Natação
 
+
+## Notas de Atualização:
+
+ - Adicionado Verificação de código para ver se o nome está vazio.
+
+ - Adicionado Verificação de código para ver se a idade está vazia.
+
+ - Adicionado Verificação de código para ver se o nome possui mais de 3 digitos.
+
+ - Adicionado Verificação de código para ver se o nome possui até de 30 digitos.
+
+ - Adicionado Verificação de código para ver se o nome possui mais de 3 digitos.
+
+ - Adicionado Verificação de código para ver se a idade está recebendo um valor numérico.
+
 ### Curso Dio PHP
 
 
@@ -12,4 +27,3 @@
 
  - Por Gabriel Arcangelo de Lima
  - https://github.com/hektorsoft
- 
--- info.php
+++ info.php
@@ -2,19 +2,51 @@
 $nome = $_POST['nome'];
 
 $idade= $_POST['idade'];
+
 $categoria = "";
 
-if ($idade>18){
-    $categoria="está na categoria: Adulto!";
+if (empty($nome)){
+
+    echo("Erro!!! O Nome não pode estar vázio.");
+    return;
 }
-elseif($idade<=18 and $idade>=13){
-    $categoria="está na categoria: Adolescente!";
+elseif (strlen($nome) < 3){
+    echo("Erro!!! O nome deve possuir ao menos 3 dígitos.");
+    return;
 }
-elseif($idade >=6 and $idade <=12 ){
-    $categoria="está na categoria: Infatil!";
+
+elseif (empty($idade)){
+
+    echo("Erro!!! A idade não pode estar vázia.");
+    return;
 }
-else
-    $categoria = "é muito novo para competir!"
+
+elseif (strlen($nome) > 16) {
+    echo("Erro!!! O nome não deve possuir mais do que 16 dígitos.");
+    return;
+}
+elseif (!is_numeric($idade)){
+
+    echo("Erro!!! O campo idade deve ser preenchido com um valor numérico.");
+    return;
+}
+
+
+else{
+
+    if ($idade>18){
+        $categoria="está na categoria: Adulto!";
+    }
+    elseif($idade<=18 and $idade>=13){
+        $categoria="está na categoria: Adolescente!";
+    }
+    elseif($idade >=6 and $idade <=12 ){
+        $categoria="está na categoria: Infatil!";
+    }
+    else{
+        $categoria = "é muito novo para competir!";
+    }
+}
 ?>
 <html>
 <center><h2>Olá <?php echo($nome) ?></h2></center>
